7596d391296908a5c5f2650bacfd6b6abf42add3
Anta <ovaorange@yandex.ru>
2024-02-29 16:28:25 +0100

    navigate with bottom menu

--- app/src/main/java/com/example/housekeeper/presentation/setting/SettingFragment.kt
+++ app/src/main/java/com/example/housekeeper/presentation/setting/SettingFragment.kt
@@ -1,6 +1,22 @@
 package com.example.housekeeper.presentation.setting
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.housekeeper.databinding.FragmentSettingBinding
 
-class SettingFragment: Fragment() {
+class SettingFragment : Fragment() {
+    private var _binding: FragmentSettingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSettingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
--- app/src/main/java/com/example/housekeeper/presentation/statistic/StatisticFragment.kt
+++ app/src/main/java/com/example/housekeeper/presentation/statistic/StatisticFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.example.housekeeper.databinding.FragmentStatisticBinding
 
 class StatisticFragment : Fragment() {
@@ -20,10 +19,4 @@ class StatisticFragment : Fragment() {
         _binding = FragmentStatisticBinding.inflate(inflater, container, false)
         return binding.root
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.backButton.setOnClickListener { findNavController().popBackStack() }
-    }
-
 }
