d50acb4d72762dfc85f880524f5b45d5de176d89
AntaOtk <ovaorange@yandex.ru>
2025-02-25 19:54:59 +0100

    new view in compose

--- app/src/main/java/com/example/housekeeper/compose/SpendingItems.kt
+++ app/src/main/java/com/example/housekeeper/compose/SpendingItems.kt
@@ -3,16 +3,18 @@ package com.example.housekeeper.compose
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.housekeeper.R
@@ -27,8 +29,11 @@ fun SpendingItem(
     Card(
         modifier = modifier
             .fillMaxWidth()
-            .padding(16.dp),
+            .padding(dimensionResource(R.dimen.small_dimen)),
         shape = RoundedCornerShape(10.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White,
+        ),
     ) {
         Row(
             modifier = modifier
@@ -38,8 +43,13 @@ fun SpendingItem(
             verticalAlignment = Alignment.CenterVertically
         ) {
             Text(text = name, style = MaterialTheme.typography.bodyMedium)
-            Text(text = content.toString(), style = MaterialTheme.typography.bodyMedium, color = colorResource(
-                R.color.purple_200))
+            Text(
+                text = content.toString(),
+                style = MaterialTheme.typography.bodyMedium,
+                color = colorResource(
+                    R.color.purple_200
+                )
+            )
         }
 
     }
--- app/src/main/java/com/example/housekeeper/presentation/main/MainScreen.kt
+++ app/src/main/java/com/example/housekeeper/presentation/main/MainScreen.kt
@@ -1,10 +1,109 @@
 package com.example.housekeeper.presentation.main
 
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.housekeeper.R
+import com.example.housekeeper.compose.ExpensesItem
+import com.example.housekeeper.compose.SpendingItem
+import org.koin.androidx.compose.koinViewModel
 
 
 @Composable
 fun MainScreen() {
+    val viewModel: MainViewModel = koinViewModel()
+    val modifier = Modifier
+    viewModel.getAccounts()
+    viewModel.getCategories()
+    val accounts = viewModel.observeAccountLiveData().observeAsState(mutableListOf())
+    val categories = viewModel.observeCategoryLiveData().observeAsState(mutableListOf())
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        Text(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(R.dimen.classic_padding)),
+            text = stringResource(R.string.app_name),
+        )
+        Card(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = dimensionResource(R.dimen.classic_padding)),
+            shape = RoundedCornerShape(10.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = colorResource(R.color.color_9),
+            ),
+        ) {
+            Column(modifier.padding(dimensionResource(R.dimen.medium_dimen)))
+            {
+                Text(stringResource(R.string.accounts))
+                Spacer(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(dimensionResource(R.dimen.small_dimen))
+                )
+                for (item in accounts.value)
+                    SpendingItem(item.name, item.sum ?: 0.0)
+            }
+        }
+        Spacer(
+            Modifier
+                .fillMaxWidth()
+                .height(dimensionResource(R.dimen.classic_padding))
+        )
+        Card(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = dimensionResource(R.dimen.classic_padding)),
+            shape = RoundedCornerShape(10.dp),
+        ) {
+            Column(modifier.padding(dimensionResource(R.dimen.medium_dimen)))
+            {
+                Text(stringResource(R.string.expenses))
+                Spacer(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(dimensionResource(R.dimen.small_dimen))
+                )
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(3),
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    items(categories.value) { item ->
+                        ExpensesItem(item.name, item.sum ?: 0.0, item.planingSum ?: 0.0, item.image)
+                    }
 
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ShowSpendingItem() {
+    MainScreen()
 }
